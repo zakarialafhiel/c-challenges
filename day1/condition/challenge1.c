d62cb0dc718f8f5b57ea5f311dfc06ce6fc61c7b
zakarialafhiel <odacchizizo@gmail.com>
2025-09-23 14:08:45 +0100

    put the variables in side for a moment and start with condictions

---
+++ day1/condition/challenge1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*Challenge 1 : Paire ou Impaire
+Écrivez un programme qui demande un nombre et affiche si ce nombre est pair ou impair.*/ 
+int main (){
+ int n;
+ if (n%2==0){
+    printf("Le nombre %d est pair",n);
+ }else{
+      printf("Le nombre %d est impair",n);
+ }
+    return 0;
+}
