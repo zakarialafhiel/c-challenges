a4230507e6741d85bb6fa0fe14812c1dc78c1b2b
zakarialafhiel <odacchizizo@gmail.com>
2025-09-23 13:42:54 +0100

    I back to solve challenge 7

--- day1/challenge 13.c
+++ day1/challenge 13.c
@@ -3,21 +3,19 @@ int main(){
   /*ktb programme y5ali utilisateur yda5l un entier ol programme y affichih bi binaire olhexadecemal
   pour demander de l'utilisateur pour entre un entier*/
 //pour donner la permission a l'utilisateur de entre un entier//
-int n,i=0;
+int n,i=0,h=0;
 printf("Entre un entier : ");
 scanf("%d",&n);
 //pour transforme la valeur de n en binair//
 printf("\n===========\n===========\n");
 printf("La valeur de %d en binar est : ",n);
+
  while(i<=n){
+ 
+
   //117=1110101//
   //i++;//
-  
-  
-  
-  
-  
-  
+
   if(n%2!=0){
     i=1;
     printf("%d",i);
@@ -25,15 +23,21 @@ printf("La valeur de %d en binar est : ",n);
   }
     
   else {
-    
     i=0;
     printf("%d",i);
      n=n/2;
   }
-
   }
+  printf("\n___________\n___________\n");
   //pour transforme la valeur de n en hexadecimal//
-    
+   while(h<=n){
+    h++;
+    if(n>0&&n<=9){
+      printf("%d",h);
+    }else{
+      if(){}
+    }
+   }   
 return 0;
 
 }
---
+++ day1/challenge7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int a,b,c;
+  printf("Entre la valur de a : ");
+  scanf("%d",&a);
+  printf("Entre la valur de b : ");
+  scanf("%d",&b);
+  printf("Entre la valur de c : ");
+  scanf("%d",&c);
+  printf("\n==========================\n");
+  printf("Le moyenne pondere de trois nombers : %d",a*2+b*3+c*5);
+    return 0;
+}
