70d67f5e192fc604dc69fdecdb74b18c27cfe285
zakarialafhiel <odacchizizo@gmail.com>
2025-09-28 18:23:52 +0100

    Just to keep the coding in habits(fonction)

---
+++ day2/Foctions/callenge2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int multiplication(int a , int b){
+  return a*b; 
+}
+int main (){
+    int a=5,b=5;
+ int  index=multiplication(a , b);
+ printf("%d",index);
+    return 0;
+}
---
+++ day2/Foctions/challenge1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+ int somme(int a  ,int b){
+   return a + b;
+ }
+ int main(){
+   int a=5,b=5;
+   int index = somme(a  , b);
+  printf("%d",index); 
+    return 0;
+ }
